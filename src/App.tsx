580ec150b6af664bde537794b6284eb77ec5740b
Andrew <andrew.eyhorn@gmail.com>
2024-11-23 20:52:09 +1100

    experimenting with getting a graph displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Graph from "graphology";
+import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
+import "@react-sigma/core/lib/react-sigma.min.css";
+
+const sigmaStyle = { height: "500px", width: "500px" };
+
+// Component that load the graph
+export const LoadGraph = () => {
+  const loadGraph = useLoadGraph();
+
+  useEffect(() => {
+    const graph = new Graph();
+    graph.addNode("first", { x: 0, y: 0, size: 15, label: "My first node", color: "#FA4F40" });
+    loadGraph(graph);
+  }, [loadGraph]);
+
+  return null;
+};
+
+// Component that display the graph
+export const DisplayGraph = () => {
+  return (
+    <SigmaContainer style={sigmaStyle}>
+      <LoadGraph />
+    </SigmaContainer>
+  );
+};
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SigmaContainer style={sigmaStyle}>
+      <LoadGraph />
+    </SigmaContainer>
   );
 }
 
-export default App;
+export default App
